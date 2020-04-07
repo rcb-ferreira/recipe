b36b7c1b3e383e04a3bf1790c288f22d18022dad
rcb-ferreira <ferreira.rcb@gmail.com>
2020-04-07 12:33:00 +0200

    feat(recipe): add all components

--- src/app/feature/recipe/recipe.module.ts
+++ src/app/feature/recipe/recipe.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeComponent } from './recipe.component';
-
-
+import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
-  declarations: [RecipeComponent],
+  declarations: [RecipeComponent, ListComponent, AddComponent, EditComponent, DetailComponent],
   imports: [
     CommonModule
   ]
